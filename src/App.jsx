aa8e746b3f709fd5c51cbb20cb250b25a0111c74
Mohedeen T <112665799+MohedeenT@users.noreply.github.com>
2023-06-29 20:57:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return <>
+    <nav className="navbar">
+  <ul className="navbar-list">
+    <li className="navbar-item"><a href="index.html">Home</a></li>
+    <li className="navbar-item"><a href="products.html">Products</a></li>
+    <li className="navbar-item"><a href="about.html">About Us</a></li>
+    <li className="navbar-item"><a href="contact.html">Contact</a></li>
+  </ul>
+</nav>
+
+  
+  </>;
+}
+
+export default App;
